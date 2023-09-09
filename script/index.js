3d07eb2b4691650d2dabf6a44e21e959523e2759
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-09-09 22:05:50 +0300

    Imporving.

--- script/index.js
+++ script/index.js
@@ -71,6 +71,18 @@ function showPage() {
 }
 window.onhashchange = () => setTimeout(showPage, 600);
 
+function createAppend(name, parent, options = {}) {
+  const elem = document.createElement(name);
+  parent.appendChild(elem);
+  for (let key in options) {
+    if (key === "txt") {
+      elem.innerText = options.txt;
+    } else {
+      elem.setAttribute(key, options[key]);
+    }
+  }
+  return elem;
+}
 function typeText(ele, str, time) {
   Array.from(str).forEach((char) => setTimeout(() => (ele.innerHTML += char), (time += 50)));
   return time;
@@ -81,20 +93,20 @@ function typeIntroduction() {
   introduction.style.zIndex = "1";
   const txt =
     "My name is Armando Kawara. I'm a full stack web developer, I do Front-end as well as Back-end development. ";
-  let span = Util.createAppend("span", introduction);
+  let span = createAppend("span", introduction);
   let time = typeText(span, txt, 2000);
-  let span1 = Util.createAppend("span", introduction);
+  let span1 = createAppend("span", introduction);
   let time1 = typeText(span1, "Feel free to take a look at my work on ", time);
-  let a2 = Util.createAppend("a", introduction, {
+  let a2 = createAppend("a", introduction, {
     href: "#portfolio",
-    class: "gen-links",
+    class: "link",
   });
   let time2 = typeText(a2, "Portfolio ", time1);
-  let span3 = Util.createAppend("span", introduction);
+  let span3 = createAppend("span", introduction);
   let time3 = typeText(span3, "page. Enquires at ", time2);
-  let a4 = Util.createAppend("a", introduction, {
+  let a4 = createAppend("a", introduction, {
     href: "mailto:info@kawaraa.com?Subject=Hello%20again",
-    class: "gen-links",
+    class: "link",
   });
   typeText(a4, "info@kawaraa.com ", time3);
 }
@@ -141,61 +153,6 @@ function animateLinks() {
   );
 }
 
-function renderProjects(techName) {
-  const container = document.getElementById("projects-results");
-  container.innerHTML = " ";
-
-  Util.fetchJSON("/api/project/" + techName)
-    .then((projects) => {
-      if (!projects[0]) return;
-      projects.forEach((project) => {
-        let div = Util.createAppend("div", container, { class: "frames" });
-        console.log(project.link);
-        Util.createAppend("iframe", div, {
-          src: project.link,
-          class: "frames-imgs",
-        });
-        Util.createAppend("a", div, {
-          href: project.link,
-          target: "_blank",
-          txt: "View",
-          class: "view-btns",
-        });
-      });
-    })
-    .catch(console.error);
-}
-
-function filterProjects(e) {
-  let width = document.getElementById("projects-categories").offsetWidth;
-  let active = document.getElementById("active-category");
-  renderProjects(e.target.name);
-
-  switch (e.target.name) {
-    case "all":
-      active.style.left = "0px";
-      break;
-    case "nodejs":
-      active.style.left = width / 6 + "px";
-      break;
-    case "javascript":
-      active.style.left = width / 3 + "px";
-      break;
-    case "react":
-      active.style.left = width / 2 + "px";
-      break;
-    case "css":
-      active.style.left = (width / 6) * 4 + "px";
-      break;
-    case "typescript":
-      active.style.left = (width / 6) * 5 + "px";
-      break;
-  }
-}
-document.querySelectorAll("#projects-categories .btns").forEach((el) => {
-  el.addEventListener("click", filterProjects);
-});
-
 function showValue(e) {
   if (e.target.name === "budget") {
     document.getElementById("budget-holder").innerHTML = "€" + e.target.value;
@@ -218,18 +175,18 @@ document.getElementById("contact-form").addEventListener("submit", (e) => {
     deadline: e.target.deadline.value,
     message: e.target.message.value,
   });
-  Util.postJSON("/api/contact", data)
-    .then((res) => {
-      e.target.style.display = "none";
-      submitResponse.style.display = "block";
-      document.getElementById("response-h2").innerHTML =
-        "Thanks for contacting me!<br />I will contact you back very soon.";
-    })
-    .catch((err) => {
-      e.target.style.display = "none";
-      submitResponse.style.display = "block";
-      document.getElementById("response-h2").innerHTML = "Something wrong happened, Please try again!";
-    });
+  // postJSON("/api/contact", data)
+  //   .then((res) => {
+  //     e.target.style.display = "none";
+  //     submitResponse.style.display = "block";
+  //     document.getElementById("response-h2").innerHTML =
+  //       "Thanks for contacting me!<br />I will contact you back very soon.";
+  //   })
+  //   .catch((err) => {
+  //     e.target.style.display = "none";
+  //     submitResponse.style.display = "block";
+  //     document.getElementById("response-h2").innerHTML = "Something wrong happened, Please try again!";
+  //   });
 
   setTimeout(() => (location.hash = "#intro"), 4000);
   setTimeout(() => {
