ebc43960d3295097258b5fbafc1de65d8d47d1bc
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-12-17 21:27:05 +0300

    Improving.

---
+++ app/[lang]/contact/contact-form.js
@@ -0,0 +1,102 @@
+"use client";
+import { send } from "../../../components/shared-content";
+import { btnCls, cardBgCls } from "../../../components/tailwindcss-class";
+const inputFieldCls = "relative mt-16";
+const underline = `outline-none border-b-[1px] border-stone-400 dark:border-gray-400 focus:border-dbg dark:focus:border-bg`;
+const inputCls = `peer block bg-transparent w-full p-1 ${underline}`;
+const inputLabelCls = "absolute top-1 left-1 z-1 peer-focus:-top-6 peer-valid:-top-6 text-lg duration-300";
+
+export default function ContactForm({ lang }) {
+  return (
+    <form className={`${cardBgCls} max-w-lg mx-auto mt-20 px-5 py-8`}>
+      <h2 className="text-3xl mb-3 text-center">{content.h2[lang]}</h2>
+
+      {content.inputs.map((input) => (
+        <div className={inputFieldCls}>
+          <input {...input.props} className={inputCls} />
+          <label for={input.props.id} className={inputLabelCls}>
+            {input.label[lang]}
+          </label>
+        </div>
+      ))}
+
+      <div className={inputFieldCls}>
+        <label for="subject" className="block mb-2">
+          {content.subjects.label[lang]}
+        </label>
+        <select name="subject" id="subject" className={`block w-full p-1 bg-transparent ${underline}`}>
+          {content.subjects.options.map((option) => (
+            <option value={option.en}>{option[lang]}</option>
+          ))}
+        </select>
+      </div>
+      {/* <div className="form-range">
+          <label for="budget">Budget</label>
+          <div className="range">
+            <input type="range" name="budget" min="0" max="10000" value="500" id="budget" />
+            <span id="budget-result-holder">€500</span>
+          </div>
+        </div>
+        <div className="form-range">
+          <label for="deadline">Deadline</label>
+          <div className="range">
+            <input type="range" name="deadline" min="0" max="365" value="5" id="deadline" />
+            <span id="deadline-result-holder">5 Days</span>
+          </div>
+        </div> */}
+      <div className={inputFieldCls}>
+        <textarea name="message" id="message" required className={`${inputCls} resize-none h-32`}></textarea>
+        <label for="message" className={inputLabelCls}>
+          {content.message[lang]}
+        </label>
+      </div>
+
+      <button type="submit" className={btnCls + " !flex w-32 mt-5 mx-auto"}>
+        {send[lang]}
+      </button>
+    </form>
+  );
+}
+
+const content = {
+  h2: { en: "Let's talk", ar: "لنتحدث معا" },
+  inputs: [
+    { label: { en: "Name", ar: "الاسم" }, props: { type: "text", name: "name", id: "name", required: true } },
+    {
+      label: { en: "Email Address", ar: "البريد الإلكتروني" },
+      props: { type: "email", name: "email", id: "email", required: true },
+    },
+    {
+      label: { en: "Company / Organization", ar: "الشركة / المنظمة" },
+      props: { type: "text", name: "organization", id: "organization", required: true },
+    },
+  ],
+  subjects: {
+    label: { en: "I need to", ar: "أنا بحاجة إلى" },
+    options: [
+      { en: "Build a Website", ar: "إنشاء موقع" },
+      { en: "rebuild a Website", ar: "إعادة بناء موقع" },
+      { en: "Update a Website", ar: "تحديث موقع" },
+      { en: "Design UI/UX", ar: "تصميم واجهة المستخدم" },
+      { en: "Build Web App", ar: "بناء تطبيق الويب" },
+      { en: "SEO improvement", ar: "تحسين أداء محرك البحث" },
+      { en: "Build an IOS App", ar: "بناء تطبيق ايفون" },
+      { en: "Update an IOS App", ar: "تحديث تطبيق ايفون" },
+      { en: "Build an Android App", ar: "بناء تطبيق أندرويد" },
+      { en: "Update an Android App", ar: "تحديث تطبيق أندرويد" },
+      { en: "Build a Windows App", ar: "بناء تطبيق ويندوز" },
+      { en: "Update a Windows App", ar: "تحديث تطبيق ويندوز" },
+      { en: "Build a MacOS App", ar: "بناء تطبيق أبل" },
+      { en: "Update a MacOS App", ar: "تحديث تطبيق أبل" },
+      { en: "Build a Restful API", ar: "بناء واجهة برمجة أو سيرفر" },
+      { en: "Build a Service", ar: "بناء خدمة" },
+      { en: "Build an entire system", ar: "بناء نظام كامل" },
+      { en: "Custom development", ar: "تطوير برمجي مخصصة" },
+      { en: "Retainer", ar: "Retainer" },
+      { en: "Partner", ar: "شريك" },
+      { en: "A volunteer", ar: "متطوع" },
+      { en: "Other Enquire", ar: "استفسار اخرى" },
+    ],
+  },
+  message: { en: "Your Message", ar: "رسالتك" },
+};
--- app/[lang]/contact/page.js
+++ app/[lang]/contact/page.js
@@ -1,4 +1,5 @@
-import { cardBgCls, h1Cls } from "../../../components/tailwindcss-class";
+import { h1Cls } from "../../../components/tailwindcss-class";
+import ContactForm from "./contact-form";
 
 export default function Contact({ params: { lang } }) {
   return (
@@ -6,15 +7,14 @@ export default function Contact({ params: { lang } }) {
       <h1 className={h1Cls}>
         {content.title[lang][0]}
         <span className="sr-only">
-          {content.title[lang][1]}
           {content.title[lang][2]}
+          {content.title[lang][3]}
         </span>
       </h1>
+
+      <p>{content.p[lang][0]}</p>
       <p>{content.p[lang][1]}</p>
-      <form className={`${cardBgCls} max-w-lg mx-auto mt-20 p-5`}>
-        <h2 className="text-3xl mt-10 mb-3 text-center">{content.title[lang][1]}</h2>
-        {/*  */}
-      </form>
+      <ContactForm lang={lang} />
     </>
   );
 }
@@ -25,16 +25,22 @@ const content = {
       "Join us on your digital journey",
       "Let's talk",
       " Contact us, Contact Kawara",
+      "Let's build something together!",
       "Are you ready to take your business to the next level?",
     ],
-    ar: "",
+    ar: [""],
   },
   p: {
     en: [
-      "Embark on your digital journey with Kawara. Whether you're looking for innovative software solutions, strategic consulting, or a reliable partner to guide your digital transformation, we are here to exceed your expectations",
-      "At Kawara, we don't just build software; we build success stories. Let's create yours together.",
+      "Whether you're looking for innovative software solutions, strategic consulting or a reliable partner to guide your digital transformation, we are here to exceed your expectations",
+      "At Kawara, we don't just build software, we build success stories. Let's create yours together.",
       "Ready to build something real together with your favorite creatives and tech leads? Get into contact or fill out an inquiry form",
+      "Contact us today, and let's embark on a journey of innovation and success.",
+      "Whether you're a startup looking to disrupt the market or an established business seeking digital transformation, we have the expertise to make it happen.",
+      "Don't miss out on the opportunity to stay ahead in the digital landscape.",
+      "Join our growing list of satisfied clients and experience the difference with Kawara.",
+      "Your success is our priority.",
     ],
-    ar: "",
+    ar: [""],
   },
 };
